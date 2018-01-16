20d2d7ea5ce4bc27776a53a7a21001b85c1aca20
Kwangsik Lee <lks21c@gmail.com>
2018-01-16 09:01:47 +0900

    terms merge developing

--- src/main/java/com/elasticsearchcache/service/CacheService.java
+++ src/main/java/com/elasticsearchcache/service/CacheService.java
@@ -360,23 +360,23 @@ public class CacheService {
                     calculateRecursively(mergedMap, termsMap);
                     logger.info("mergedMap = " + JsonUtil.convertAsString(mergedMap));
 
-//                    if (mergedBucket.size() == 0) {
-//                        mergedBucket = (List<Map<String, Object>>) SerializationUtils.clone(new ArrayList<>(termsBuckets));
-//                    } else {
-//                        for (Map<String, Object> bkt : termsBuckets) {
-//                            String key = (String) bkt.get("key");
-//                            double docCnt = (double) bkt.get("doc_count");
-//
-//                            for (Map<String, Object> mBkt : mergedBucket) {
-//                                String mKey = (String) mBkt.get("key");
-//                                double mDocCnt = (double) mBkt.get("doc_count");
-//                                if (mKey.equals(key)) {
-//                                    mDocCnt += docCnt;
-//                                    mBkt.put("doc_count", mDocCnt);
-//                                }
-//                            }
-//                        }
-//                    }
+                    if (mergedBucket.size() == 0) {
+                        mergedBucket = (List<Map<String, Object>>) SerializationUtils.clone(new ArrayList<>(termsBuckets));
+                    } else {
+                        for (Map<String, Object> bkt : termsBuckets) {
+                            String key = (String) bkt.get("key");
+                            double docCnt = (double) bkt.get("doc_count");
+
+                            for (Map<String, Object> mBkt : mergedBucket) {
+                                String mKey = (String) mBkt.get("key");
+                                double mDocCnt = (double) mBkt.get("doc_count");
+                                if (mKey.equals(key)) {
+                                    mDocCnt += docCnt;
+                                    mBkt.put("doc_count", mDocCnt);
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -409,6 +409,7 @@ public class CacheService {
             for (Map<String, Object> mergedBucket : mergedBucketList) {
                 if (bucket.get("key").toString().equals(mergedBucket.get("key").toString())) {
                     notExists = false;
+                    calculateRecursively(mergedBucket,bucket);
                 }
             }
             if (notExists) {
