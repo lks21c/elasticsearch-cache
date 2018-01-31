b8111593fb2605fc2ba23ca019913d3192a06700
lks21c <lks21c@gmail.com>
2018-01-31 18:24:30 +0900

    add test property and modify warmup test

--- src/test/java/com/elasticsearchcache/WarmUpServiceTest.java
+++ src/test/java/com/elasticsearchcache/WarmUpServiceTest.java
@@ -6,9 +6,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@TestPropertySource("classpath:application_test.properties")
 @SpringBootTest
 public class WarmUpServiceTest {
     @Autowired
