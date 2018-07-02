47bbd5a973fde1d73abefdb02b4dbac3183bd87e
Kwangsiklee <lks21c@gmail.com>
2018-07-02 16:16:41 +0900

    add JsonUtil logging

--- src/main/java/com/elasticsearchcache/util/JsonUtil.java
+++ src/main/java/com/elasticsearchcache/util/JsonUtil.java
@@ -2,16 +2,21 @@ package com.elasticsearchcache.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JsonUtil {
+    private static Logger logger = LoggerFactory.getLogger(JsonUtil.class);
+
     private static ObjectMapper mapper = new ObjectMapper();
 
     public static String convertAsString(Object value) {
         try {
             return mapper.writeValueAsString(value);
         } catch (JsonProcessingException e) {
+            logger.error(e.getMessage());
             e.printStackTrace();
             return null;
         }
